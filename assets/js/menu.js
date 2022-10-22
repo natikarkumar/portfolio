a5cf6a07bd23cd1087de309c86c3124889ceb830
Kumar Natikar <45071886+natikarkumar@users.noreply.github.com>
2022-10-22 11:08:42 +0530

    added feature

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,7 +8,20 @@ $(function(){
         return false;
     });
 });
-
+$(function(){ 
+    $('.SmoothmyTop').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 1000);
+      return false;
+  });
+});
+$(window).scroll(function() {
+    if ($(this).scrollTop()>200){
+        $('.SmoothmyTop').show(1000);
+     }
+    else{
+      $('.SmoothmyTop').hide(1000);
+     }
+ });
 //Fixed-top menu
 function fixedHeader() {
     var ww = $(window).scrollTop();
